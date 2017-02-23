788932c622565a676240252a0f24c7f8e98bfc0d
Michelangelo Accettura III <michelangelo.accettura@gmail.com>
2017-02-22 20:29:45 -0500

    Cleanup web project

--- Polypic.Web/Controllers/HomeController.cs
+++ Polypic.Web/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using ImageSharp;
 using Microsoft.AspNetCore.Mvc;
 using Polypic.Generate;
 using Polypic.Generate.Model;
@@ -12,61 +8,31 @@ namespace Polypic.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly Random Random = new Random();
-
         public async Task<IActionResult> Index(int? width, int? height, string color1, string color2, int? steps, string source)
         {
+            //Create request object
             var request = new ImageRequest()
             {
                 Width = width ?? 100,
                 Height = height ?? 100,
                 Steps = steps ?? 5,
-                Source = source
+                Source = source,
+                Color1 = color1,
+                Color2 = color2
             };
 
-            if (!IsHexColor(color1) || !IsHexColor(color2))
+            if (!request.IsEmptyColors() && !request.IsValidColors())
             {
-                Tuple<string, string> hexPair = GetRandomComplmentaryHexPair();
-                request.Color1 = !IsHexColor(color1) ? hexPair.Item1 : color1;
-                request.Color2 = !IsHexColor(color2) ? hexPair.Item2 : color2;
+                return RedirectToAction("Error");
             }
 
             Tuple<string, byte[]> image = await ImageGenerator.GenerateAsync(request);
             return File(image.Item2, image.Item1);
         }
-        
-        private static Tuple<string, string> GetRandomComplmentaryHexPair()
-        {
-            string hex1 = string.Empty;
-            string hex2 = string.Empty;
-            for (var i = 0; i < 6; i++)
-            {
-                int val = Random.Next(0, 15);
-                hex1 += val.ToString("X");
-                hex2 += (15 - val).ToString("X");
-            }
-            return new Tuple<string, string>(hex1, hex2);
-        }
 
-        private static bool IsHexColor(string input)
+        public IActionResult Error()
         {
-            if (string.IsNullOrWhiteSpace(input) || input .Length != 3 || input.Length != 6)
-            {
-                return false;
-            }
-            for (var index = 0; index < input.ToCharArray().Length; index++)
-            {
-                char c = input[index];
-                bool isHex = ((c >= '0' && c <= '9') ||
-                              (c >= 'a' && c <= 'f') ||
-                              (c >= 'A' && c <= 'F'));
-
-                if (!isHex)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+            return View();
+        }  
     }
 }
